34effebfc5c17c9ce69ee0393eee23a3d434feae
homfarnam <homfarnam@gmail.com>
2021-09-10 22:28:36 +0430

    Create use char origin custom hook

---
+++ src/hooks/useCharOrigin.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { OriginTypes } from '../interfaces/types';
+
+const useCharOrigin = (url: string) => {
+  const [errorOrigin, setErrorOrigin] = useState('');
+  const [loadingOrigin, setLoadingOrigin] = useState(true);
+  const [originData, setOriginData] = useState<OriginTypes>();
+  const fetchData = (url: string) => {
+    axios
+      .get<OriginTypes>(url)
+      .then((res) => {
+        setOriginData(res.data);
+      })
+      .catch((err) => {
+        setErrorOrigin('There is no origin url');
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setLoadingOrigin(false);
+        }, 1500);
+      });
+  };
+
+  useEffect(() => {
+    if (url) {
+      fetchData(url);
+    }
+  }, [url]);
+
+  // custom hook returns value
+  return { originData, errorOrigin, loadingOrigin };
+};
+
+export default useCharOrigin;
