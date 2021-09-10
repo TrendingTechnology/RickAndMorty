cb97c362236971a5ab0c82d1e0dcf30067e9f2cc
homfarnam <homfarnam@gmail.com>
2021-09-10 22:29:05 +0430

    Use hooks for fetch data

--- src/pages/Charachter.tsx
+++ src/pages/Charachter.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useHistory, useParams } from 'react-router';
+import { RouteComponentProps, useHistory } from 'react-router';
 import useAxios from '../hooks/useAxios';
-import { LocationTypes, OriginTypes } from '../interfaces/types';
 import Loader from 'react-loader-spinner';
 import { ImArrowLeft } from 'react-icons/im';
 import CharDetail from '../components/CharDetail/CharDetail';
+import useCharLocation from '../hooks/useCharLocation';
+import useCharOrigin from '../hooks/useCharOrigin';
+import useEpisodes from '../hooks/useEpisodes';
 
 type EpisodesType = {
   id: number;
@@ -14,74 +15,24 @@ type EpisodesType = {
   date: string;
 };
 
-const Charachter = () => {
-  const [locationData, setLocationData] = useState<LocationTypes>();
-  const [originData, setOriginData] = useState<OriginTypes>();
+const Charachter = (props: RouteComponentProps<{ id?: string }>) => {
   const [episodes, setEpisodes] = useState<string[]>([]);
-  const [episodesData, setEpisodesData] = useState<EpisodesType[]>();
 
-  const { id }: any = useParams();
+  const id = Number(props.match.params.id);
+
   const history = useHistory();
 
   const { response, loading, error, location, origin } = useAxios(id);
 
+  const { locationData, errorLocation } = useCharLocation(location);
+
+  const { originData, errorOrigin } = useCharOrigin(origin);
+
+  const { episodesData, errorEpisodes } = useEpisodes(episodes);
+
   useEffect(() => {
     setEpisodes(response?.episode as string[]);
   }, [id, response]);
-
-  const fetchLocation = async (location: string) => {
-    await axios
-      .get<LocationTypes>(location)
-      .then((res) => {
-        setLocationData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const fetchOrigin = async (origin: string) => {
-    await axios
-      .get<OriginTypes>(origin)
-      .then((res) => {
-        setOriginData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const fetchAllEpisodes = () => {
-    if (episodes?.length > 0) {
-      let allEp = episodes.map((ep) => axios.get(ep));
-
-      axios
-        .all(allEp)
-        .then(
-          axios.spread((...allData) => {
-            const data = allData.map((item) => ({
-              id: item?.data?.id,
-              name: item?.data?.name,
-              date: item.data.air_date,
-            }));
-            setEpisodesData(data);
-          })
-        )
-        .catch((err) => console.log(err));
-    }
-  };
-
-  useEffect(() => {
-    fetchAllEpisodes();
-  }, [id, episodes]);
-
-  useEffect(() => {
-    fetchLocation(location);
-  }, [id, location]);
-
-  useEffect(() => {
-    fetchOrigin(origin);
-  }, [id, origin]);
 
   const handleHomePage = () => {
     history.push('/');
@@ -125,49 +76,75 @@ const Charachter = () => {
                   <CharDetail data={response?.status} title='Status: ' />
                   <CharDetail data={response?.name} title='Location: ' />
 
-                  <div className='flex flex-col space-y-3'>
-                    <CharDetail
-                      data={locationData?.residents?.length as number}
-                      title='Amount of residents: '
-                      list
-                    />
+                  {errorLocation ? (
+                    <span>{errorLocation}</span>
+                  ) : (
+                    locationData && (
+                      <div className='flex flex-col space-y-3'>
+                        <CharDetail
+                          data={locationData?.residents?.length as number}
+                          title='Amount of residents: '
+                          list
+                        />
 
-                    <CharDetail data={locationData?.dimension as string} title='Dimension: ' list />
-                    <CharDetail data={locationData?.type as string} title='Type: ' list />
-                  </div>
+                        <CharDetail
+                          data={locationData?.dimension as string}
+                          title='Dimension: '
+                          list
+                        />
+                        <CharDetail data={locationData?.type as string} title='Type: ' list />
+                      </div>
+                    )
+                  )}
 
                   <CharDetail data={response?.origin.name} className='pt-5' title='Origin: ' />
-
-                  <div className='flex flex-col space-y-3'>
-                    <CharDetail
-                      data={originData?.residents?.length as number}
-                      title='Amount of residents: '
-                      list
-                    />
-                    <CharDetail data={originData?.dimension as string} title='Dimension: ' list />
-                    <CharDetail data={originData?.type as string} title='Type: ' list />
-                  </div>
+                  {errorOrigin ? (
+                    <span>{errorOrigin}</span>
+                  ) : (
+                    originData && (
+                      <div className='flex flex-col space-y-3'>
+                        <CharDetail
+                          data={originData?.residents?.length as number}
+                          title='Amount of residents: '
+                          list
+                        />
+                        <CharDetail
+                          data={originData?.dimension as string}
+                          title='Dimension: '
+                          list
+                        />
+                        <CharDetail data={originData?.type as string} title='Type: ' list />
+                      </div>
+                    )
+                  )}
 
                   <div className='pt-10 mx-10 md:mx-auto'>
                     <span className='my-3 flex justify-center items-center text-xl font-medium'>
                       Chapters that {response.name} featured on{' '}
                     </span>
-                    <table className='border border-black'>
-                      <thead className='p-2 uppercase'>
-                        <th className=''>Id</th>
-                        <th>Name</th>
-                        <th>Date</th>
-                      </thead>
-                      <tbody className='p-2'>
-                        {episodesData?.map((item: EpisodesType, i: number) => (
-                          <tr className='text-center cursor-pointer' key={item?.id}>
-                            <td className='bg-blue-400'>{item.id}</td>
-                            <td className='bg-yellow-500'>{item?.name}</td>
-                            <td className='bg-yellow-600'>{item?.date}</td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
+
+                    {errorEpisodes ? (
+                      <span>{errorEpisodes}</span>
+                    ) : (
+                      episodesData && (
+                        <table className='border border-black'>
+                          <thead className='p-2 uppercase'>
+                            <th className=''>Id</th>
+                            <th>Name</th>
+                            <th>Date</th>
+                          </thead>
+                          <tbody className='p-2'>
+                            {episodesData?.map((item: EpisodesType, i: number) => (
+                              <tr className='text-center cursor-pointer' key={item?.id}>
+                                <td className='bg-blue-400'>{item.id}</td>
+                                <td className='bg-yellow-500'>{item?.name}</td>
+                                <td className='bg-yellow-600'>{item?.date}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      )
+                    )}
                   </div>
                 </div>
               </div>
@@ -179,4 +156,4 @@ const Charachter = () => {
   );
 };
 
-export default Charachter;
+export default React.memo(Charachter);
