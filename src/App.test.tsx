653606c23ee8639a6617e44e3a2ab40015bc25eb
homfarnam <homfarnam@gmail.com>
2021-09-11 00:46:45 +0430

    Add test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { cleanup, render } from "@testing-library/react"
 import App from "./App"
+import Home from "./pages/Home"
+import Enzyme, { mount } from "enzyme"
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
+import toJson from "enzyme-to-json"
+import { Router } from "react-router-dom"
+import { createMemoryHistory } from "history"
+import CharDetail from "./components/CharDetail/CharDetail"
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+Enzyme.configure({ adapter: new Adapter() })
+afterEach(cleanup)
+
+describe("snapshot test", () => {
+  const history = createMemoryHistory()
+
+  it("snapshot test Home page", () => {
+    const wrapper = Enzyme.shallow(<Home />)
+
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it("renders text in home page", () => {
+    const { container } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    )
+
+    expect(container.textContent).toMatch("Rick and Morty Charchters")
+  })
 })
