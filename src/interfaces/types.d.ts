ba647db74e87848cf51a672d78f5fd90adfa501d
homfarnam <homfarnam@gmail.com>
2021-09-12 22:58:42 +0430

    Change type

--- src/interfaces/types.d.ts
+++ src/interfaces/types.d.ts
@@ -1,21 +1,22 @@
 interface Character {
-  created: Date
-  episode: string[]
-  gender: string
   id: number
-  image: string
-  location: {
-    name: string
-    url: string
-  }
   name: string
+  status: string
+  species: string
+  type: string
+  gender: string
   origin: {
     name: string
     url: string
   }
-  species: string
-  status: string
-  type: string
+  location: {
+    name: string
+    url: string
+  }
+  image: string
+  episode: string[]
+  url: string
+  created: string
 }
 
 interface DataTypes {
